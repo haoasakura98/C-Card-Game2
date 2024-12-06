2d41c593282d3f758cdec73b7ad6dde115a15813
haoasakura98 <rafaelmsilva98@gmail.com>
2024-12-06 21:58:15 +0000

    adicionado menu principal, menu para escolha atrib

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,149 +1,168 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
 
-    int codigo, pontos;
-    char nome[30];
-    float populacao;
-    double area, PIB;
+    // Menu inicial
+    int menu;
 
-    //Variaveis da segunda carta.
+    printf("Menu SuperTrunfo\n");
+    printf("1. Jogar  2. Regras  3. Sair\n");
+    scanf("%d", &menu);
 
-    int codigo2, pontos2;
-    char nome2[30];
-    float populacao2;
-    double area2, PIB2;
+    switch(menu) {
+        case 1:
+        // Definição das variáveis para armazenar as propriedades das cidades
+        {
+            int codigo, pontos;
+            char nome[30];
+            float populacao;
+            double area, PIB;
 
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+            // Variáveis da segunda carta.
+            int codigo2, pontos2;
+            char nome2[30];
+            float populacao2;
+            double area2, PIB2;
 
-    printf("\n***Registro de dados da primeira carta***\n");
-    printf("Digite o codigo da cidade:\n");
-    scanf("%d", &codigo);
+            // Cadastro da primeira Carta:
+            printf("\n***Registro de dados da primeira carta***\n");
+            printf("Digite o codigo da cidade:\n");
+            scanf("%d", &codigo);
 
-    printf("Digite o número de pontos turísticos:\n");
-    scanf("%d", &pontos);
+            printf("Digite o número de pontos turísticos:\n");
+            scanf("%d", &pontos);
 
-    printf("Agora digite o nome da cidade:\n");
-    scanf("%s", nome);
+            printf("Agora digite o nome da cidade:\n");
+            scanf("%s", nome);
 
-    printf("Agora digite a população: \n");
-    scanf("%f", &populacao);
+            printf("Agora digite a população: \n");
+            scanf("%f", &populacao);
 
-    printf("Agora digite a área:\n");
-    scanf("%lf", &area);
+            printf("Agora digite a área:\n");
+            scanf("%lf", &area);
 
-    printf("Agora digite o PIB:\n");
-    scanf("%lf", &PIB);
+            printf("Agora digite o PIB:\n");
+            scanf("%lf", &PIB);
 
-    //CADASTRO DA SEGUNDA CARTA
+            // Cadastro da segunda carta de forma aleatória
+            printf("Qual o nome da segunda carta?\n");
+            scanf("%s", nome2);
 
-    printf("\n***Registro de dados da segunda carta***\n");
-    printf("Digite o codigo da segunda cidade:\n");
-    scanf("%d", &codigo2);
+            srand(time(0));
+            codigo2 = rand() % 9 + 1;
+            pontos2 = rand() % 9 + 1;
+            populacao2 = rand() % 999;
+            area2 = rand() % 999;
+            PIB2 = rand() % 999;
 
-    printf("Digite o segundo número de pontos turísticos:\n");
-    scanf("%d", &pontos2);
+            // Cálculo do PIB per capita para ambas as cidades
+            float PIBperCAPITA = (float)PIB / populacao;
+            float PIBperCAPITA2 = (float)PIB2 / populacao2;
 
-    printf("Agora digite o segundo nome da cidade:\n");
-    scanf("%s", nome2);
+            // Cálculo da densidade para ambas as cidades
+            float densidade = populacao / (float)area;
+            float densidade2 = populacao2 / (float)area2;
 
-    printf("Agora digite a segunda população: \n");
-    scanf("%f", &populacao2);
+            printf("\n***As cartas foram cadastradas com sucesso***\n");
 
-    printf("Agora digite a segunda área:\n");
-    scanf("%lf", &area2);
+            // ESCOLHA DOS ATRIBUTOS A SEREM DISPUTADOS.
+            int atributo1, atributo2;
+            printf("Escolha dois critérios que serão disputados pelas cartas\n");
+            printf("1. Codigo 2. Pontos 3. População 4. Área 5. PIBperCAPITA 6. Densidade");
+            scanf("%d", &atributo1);
+            printf("Agora escolha o segundo atributo.\n");
+            scanf("%d", &atributo2);
 
-    printf("Agora digite o segundo PIB:\n");
-    scanf("%lf", &PIB2);   
-    
-    printf("\n***As cartas foram cadastradas com sucesso***\n");
+            // Exibição dos Dados das Cartas:
+            printf("\n***Dados da primeira carta***\n");
+            printf("Cidade: %s\n Codigo: %d\n Pontos: %d\n População: %.3f\n Área: %.3lf\n PIB: %.3lf\n PIB per Capita: %.3f Densidade: %.3f\n", nome, codigo, pontos, populacao, area, PIB, PIBperCAPITA, densidade);
+            printf("\n***Dados da segunda carta***\n");
+            printf("Cidade: %s\n Codigo: %d\n Pontos: %d\n População: %.3f\n Área: %.3lf\n PIB: %.3lf\n PIB per Capita: %.3f Densidade: %.3f\n", nome2, codigo2, pontos2, populacao2, area2, PIB2, PIBperCAPITA2, densidade2);
 
-    // Novas Propriedades Calculadas:
-    //Densidade Populacional: População dividida pela área da cidade.
-    //PIB per Capita: PIB total dividido pela população.
+            // Inicializando variáveis de controle para vitória nos critérios
+            int venceu_atributo1 = 0;
+            int venceu_atributo2 = 0;
 
-    float densidade = (float) populacao / area;
-    float PIBperCAPITA = (float) PIB / populacao;
-    
+            // Comparação de Cartas através de operador ternário
+            // o valor de venceu_atributo será 1 caso a primeira carta ganhe e -1 caso a segunda carta ganhe
+            switch(atributo1) {
+                case 1:
+                    venceu_atributo1 = (codigo > codigo2) ? 1 : (codigo < codigo2) ? -1 : 0;
+                    break;
+                case 2:
+                    venceu_atributo1 = (pontos > pontos2) ? 1 : (pontos < pontos2) ? -1 : 0;
+                    break;
+                case 3:
+                    venceu_atributo1 = (populacao > populacao2) ? 1 : (populacao < populacao2) ? -1 : 0;
+                    break;
+                case 4:
+                    venceu_atributo1 = (area > area2) ? 1 : (area < area2) ? -1 : 0;
+                    break;
+                case 5:
+                    venceu_atributo1 = (PIBperCAPITA > PIBperCAPITA2) ? 1 : (PIBperCAPITA < PIBperCAPITA2) ? -1 : 0;
+                    break;
+                case 6:
+                    venceu_atributo1 = (densidade > densidade2) ? 1 : (densidade < densidade2) ? -1 : 0;
+                    break;
+                default:
+                    printf("opcao invalida");
+                    return 0;
+            }
 
-    float densidade2 = (float) populacao2 / area2;
-    float PIBperCAPITA2 = (float) PIB2 / populacao2;
+            switch(atributo2) {
+                case 1:
+                    venceu_atributo2 = (codigo > codigo2) ? 1 : (codigo < codigo2) ? -1 : 0;
+                    break;
+                case 2:
+                    venceu_atributo2 = (pontos > pontos2) ? 1 : (pontos < pontos2) ? -1 : 0;
+                    break;
+                case 3:
+                    venceu_atributo2 = (populacao > populacao2) ? 1 : (populacao < populacao2) ? -1 : 0;
+                    break;
+                case 4:
+                    venceu_atributo2 = (area > area2) ? 1 : (area < area2) ? -1 : 0;
+                    break;
+                case 5:
+                    venceu_atributo2 = (PIBperCAPITA > PIBperCAPITA2) ? 1 : (PIBperCAPITA < PIBperCAPITA2) ? -1 : 0;
+                    break;
+                case 6:
+                    venceu_atributo2 = (densidade > densidade2) ? 1 : (densidade < densidade2) ? -1 : 0;
+                    break;
+                default:
+                    printf("opcao invalida");
+                    return 0;
+            }
 
+            // Exibição do Vencedor baseado nos dois critérios:
+            printf("\nComparando a área e o PIB per capita das cartas...\n");
 
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+            if (venceu_atributo1 == 1 && venceu_atributo2 == 1) {
+                printf("\nA carta 1 venceu nos dois critérios!\n");
+            } else if (venceu_atributo1 == -1 && venceu_atributo2 == -1) {
+                printf("\nA carta 2 venceu nos dois critérios!\n");
+            } else {
+                printf("\nEmpate! Cada carta venceu em um critério diferente.\n");
+            }
+        }
+        break;
 
-    printf("\n***Dados da primeira carta***\n");
-    printf("Cidade: %s\n Codigo: %d\n Pontos: %d\n População: %.3f\n Área: %.3lf\n PIB: %.3lf\n Densidade: %.3f\n PIB per Capita de: %.3f\n", nome, codigo, pontos, populacao, area, PIB, densidade, PIBperCAPITA);
-    printf("\n***Dados da segunda carta***\n");
-    printf("Cidade: %s\n Codigo: %d\n Pontos: %d\n População: %.3f\n Área: %.3lf\n PIB: %.3lf\n Densidade: %.3f\n PIB per Capita de: %.3f\n", nome2, codigo2, pontos2, populacao2, area2, PIB2, densidade2, PIBperCAPITA2);
+        case 2:
+            printf("\nRegras do jogo:\n");
+            printf("1. Cada carta possui atributos como população, área, PIB, PIB per capita, densidade, código e pontos turísticos.\n");
+            printf("2. O vencedor é a carta que ganhar nos dois atributos escolhidos pelo jogador.\n");
+            printf("3. Empate ocorre se cada carta vencer em um critério.\n");
+        break;
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+        case 3:
+            return 0;
+        break;
 
-    if (area > area2) {
-        printf("Carta 1 possui area maior\n");
-    } else if (area < area2) {
-        printf("Carta 2 possui area maior\n");
-    } else {
-        printf("As duas cartas possuem a mesma area\n");
+        default:
+            printf("Opção inválida\n");
+        break;
     }
-
-        if (PIBperCAPITA > PIBperCAPITA2) {
-        printf("Carta 1 possui PIB per capita maior\n");
-    } else if (area < area2) {
-        printf("Carta 2 possui pib per capita maior\n");
-    } else {
-        printf("As duas cartas possuem o mesmo PIB per capita\n");
-    }
-
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Inicializando variáveis de controle para vitória nos critérios
-    int venceu_area = 0;
-    int venceu_pib = 0;
-    
-    // Comparando a área das cidades
-    if (area > area2) {
-        venceu_area = 1;  // Carta 1 venceu na área
-    } else if (area < area2) {
-        venceu_area = -1;  // Carta 2 venceu na área
-    }
-
-    // Comparando o PIB per capita das cidades
-    if (PIBperCAPITA > PIBperCAPITA2) {
-        venceu_pib = 1;  // Carta 1 venceu no PIB per capita
-    } else if (PIBperCAPITA < PIBperCAPITA2) {
-        venceu_pib = -1;  // Carta 2 venceu no PIB per capita
-    }
-
-    // Exibição do Vencedor baseado nos dois critérios:
-    printf("\nComparando a area e o PIB per capita das cartas...\n");
-    
-    if (venceu_area == 1 && venceu_pib == 1) {
-        printf("\nA carta 1 venceu nos dois critérios!\n");
-    } else if (venceu_area == -1 && venceu_pib == -1) {
-        printf("\nA carta 2 venceu nos dois critérios!\n");
-    } else {
-        printf("\nEmpate! Cada carta venceu em um critério diferente.\n");
-    }
-
- 
 
     return 0;
 }
